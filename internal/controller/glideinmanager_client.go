c7e6a551696fe6987b446741ae8697945dd41a9e
Matthew Westphall <westphall@wisc.edu>
2024-06-26 15:40:47 -0500

    update glidein manager watchers to support driving multiple namespaces from a single glidein manager

--- internal/controller/glideinmanager_client.go
+++ internal/controller/glideinmanager_client.go
@@ -15,13 +15,14 @@ import (
 type UpdateCallback func(gmosClient.RepoUpdate) error
 
 type GlideinManagerPoller struct {
-	onUpdate UpdateCallback
-	client   *gmosClient.GlideinManagerClient
-	ticker   *time.Ticker
-	doneChan chan (bool)
+	updateCallbacks map[string]UpdateCallback
+	client          *gmosClient.GlideinManagerClient
+	gitPullTicker   *time.Ticker
+	refreshTicker   *time.Ticker
+	doneChan        chan (bool)
 }
 
-func NewGlidenManagerPoller(clientName, managerUrl string, updateCallback UpdateCallback) *GlideinManagerPoller {
+func NewGlidenManagerPoller(clientName string, managerUrl string) *GlideinManagerPoller {
 	client := &gmosClient.GlideinManagerClient{
 		HostName:   clientName,
 		ManagerUrl: managerUrl,
@@ -29,28 +30,50 @@ func NewGlidenManagerPoller(clientName, managerUrl string, updateCallback Update
 	}
 
 	poller := &GlideinManagerPoller{
-		onUpdate: updateCallback,
-		client:   client,
-		doneChan: make(chan bool),
+		updateCallbacks: make(map[string]UpdateCallback),
+		client:          client,
+		doneChan:        make(chan bool),
 	}
 	return poller
 }
 
-func (p *GlideinManagerPoller) StartPolling(interval time.Duration) {
-	if p.ticker != nil {
+func (p *GlideinManagerPoller) StartPolling(pollInterval time.Duration, refreshInterval time.Duration) {
+	if p.gitPullTicker != nil || p.refreshTicker != nil {
 		return
 	}
-	p.ticker = time.NewTicker(interval)
+	p.gitPullTicker = time.NewTicker(pollInterval)
 	go func() {
 		for {
 			select {
 			case <-p.doneChan:
 				return
-			case <-p.ticker.C:
+			case <-p.gitPullTicker.C:
 				p.CheckForGitUpdates()
 			}
 		}
 	}()
+
+	p.refreshTicker = time.NewTicker(refreshInterval)
+	go func() {
+		for {
+			select {
+			case <-p.doneChan:
+				return
+			case <-p.refreshTicker.C:
+				if err := p.DoHandshakeWithRetry(15, 5*time.Second); err != nil {
+					return
+				}
+			}
+		}
+	}()
+}
+func (p *GlideinManagerPoller) AddCallback(namespace string, callback UpdateCallback) {
+	p.updateCallbacks[namespace] = callback
+}
+
+func (p *GlideinManagerPoller) HasCallbackForNamespace(namespace string) bool {
+	_, exists := p.updateCallbacks[namespace]
+	return exists
 }
 
 func (p *GlideinManagerPoller) CheckForGitUpdates() {
@@ -63,8 +86,10 @@ func (p *GlideinManagerPoller) CheckForGitUpdates() {
 	}
 	if repoUpdate.Updated() {
 		log.Info(fmt.Sprintf("Updated to commit %v", repoUpdate.CurrentCommit))
-		if err := p.onUpdate(repoUpdate); err != nil {
-			log.Error(err, "Error occurred while handling repo update")
+		for namespace, callback := range p.updateCallbacks {
+			if err := callback(repoUpdate); err != nil {
+				log.Error(err, "Error occurred while handling repo update for namespace %v", namespace)
+			}
 		}
 	} else {
 		log.Info("No updates to repo")
@@ -73,16 +98,18 @@ func (p *GlideinManagerPoller) CheckForGitUpdates() {
 }
 
 func (p *GlideinManagerPoller) StopPolling() {
-	p.ticker.Stop()
+	p.gitPullTicker.Stop()
+	p.refreshTicker.Stop()
 	p.doneChan <- true
 }
 
 func (p *GlideinManagerPoller) DoHandshakeWithRetry(retries int, delay time.Duration) error {
+	log := log.FromContext(context.TODO())
+	log.Info("Doing handshake with Glidein Manager Object Server")
 	errs := []error{}
-	fmt.Printf("Doing handshake with Glidein Manager Object Server")
 	for i := 0; i < retries; i++ {
 		if err := p.client.DoHandshake(8071); err != nil {
-			fmt.Printf("handshake with GMOS failed: %v", err)
+			log.Error(err, "handshake with GMOS failed")
 			errs = append(errs, err)
 			time.Sleep(delay)
 		} else {
@@ -95,29 +122,52 @@ func (p *GlideinManagerPoller) DoHandshakeWithRetry(retries int, delay time.Dura
 var activeGlideinManagerPollers = make(map[string]*GlideinManagerPoller)
 
 func AddGlideinManagerWatcher(pilotSet *gmosv1alpha1.GlideinManagerPilotSet, updateCallback UpdateCallback) error {
+	log := log.FromContext(context.TODO())
+	log.Info(fmt.Sprintf("Updating Glidein Manager Watcher for namespace %v", pilotSet.Namespace))
+
 	clientName, ok := os.LookupEnv("CLIENT_NAME")
 	if !ok {
 		return errors.New("env var CLIENT_NAME missing")
 	}
 
-	if _, exists := activeGlideinManagerPollers[pilotSet.Namespace]; !exists {
-		poller := NewGlidenManagerPoller(clientName, pilotSet.Spec.GlideinManagerUrl, updateCallback)
-		activeGlideinManagerPollers[pilotSet.Namespace] = poller
+	if existingPoller, exists := activeGlideinManagerPollers[pilotSet.Spec.GlideinManagerUrl]; !exists {
+		log.Info(fmt.Sprintf("No existing watchers for manager %v. Creating for namespace %v", pilotSet.Spec.GlideinManagerUrl, pilotSet.Namespace))
+		poller := NewGlidenManagerPoller(clientName, pilotSet.Spec.GlideinManagerUrl)
+		poller.AddCallback(pilotSet.Namespace, updateCallback)
+		activeGlideinManagerPollers[pilotSet.Spec.GlideinManagerUrl] = poller
 		go func() {
 			if err := poller.DoHandshakeWithRetry(15, 5*time.Second); err != nil {
-				fmt.Printf("Unable to complete handshake with GMOS")
+				log.Error(err, "Unable to complete handshake with GMOS")
 				return
 			}
-			poller.StartPolling(1 * time.Minute)
+			poller.StartPolling(1*time.Minute, 1*time.Hour)
 		}()
 
+	} else if !existingPoller.HasCallbackForNamespace(pilotSet.Namespace) {
+		// remove the client from other namespaces
+		RemoveGlideinManagerWatcher(pilotSet)
+		existingPoller.AddCallback(pilotSet.Namespace, updateCallback)
 	}
 	return nil
 }
 
 func RemoveGlideinManagerWatcher(pilotSet *gmosv1alpha1.GlideinManagerPilotSet) {
-	if poller, exists := activeGlideinManagerPollers[pilotSet.Namespace]; exists {
-		delete(activeGlideinManagerPollers, pilotSet.Namespace)
-		poller.StopPolling()
+	log := log.FromContext(context.TODO())
+	log.Info(fmt.Sprintf("Removing glidein manager watcher from namespace %v", pilotSet.Namespace))
+	var toDelete *GlideinManagerPoller = nil
+	for _, poller := range activeGlideinManagerPollers {
+		if poller.HasCallbackForNamespace(pilotSet.Namespace) {
+			log.Info(fmt.Sprintf("consumer removed for manager %v", poller.client.ManagerUrl))
+			delete(poller.updateCallbacks, pilotSet.Namespace)
+			if len(poller.updateCallbacks) == 0 {
+				toDelete = poller
+			}
+			break
+		}
+	}
+	if toDelete != nil {
+		log.Info(fmt.Sprintf("Last consumer removed for manager %v. Removing watcher.", toDelete.client.ManagerUrl))
+		delete(activeGlideinManagerPollers, toDelete.client.ManagerUrl)
+		toDelete.StopPolling()
 	}
 }
